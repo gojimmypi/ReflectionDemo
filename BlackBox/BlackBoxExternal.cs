4975e24e14d606ae0ff9c1bb34ef65e08095b603
gojimmypi <gojimmypi@gmail.com>
2018-07-22 14:13:55 -0700

    add features for stackframe inspection and programmatic source code viewing

--- BlackBox/BlackBoxExternal.cs
+++ BlackBox/BlackBoxExternal.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Text;
+using System.Diagnostics;
 using System.Reflection;
 using System.Configuration;
-
+using System.Diagnostics.SymbolStore;
+using System.Runtime.InteropServices;
 
 namespace BlackBoxDemo
 {
@@ -11,6 +14,82 @@ namespace BlackBoxDemo
     {
         private readonly string MainExecutableName = System.Reflection.Assembly.GetEntryAssembly().Location; // or  AppContext.BaseDirectory + AppDomain.CurrentDomain.FriendlyName; 
         private readonly string thisCodeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+
+        // from https://social.msdn.microsoft.com/Forums/vstudio/en-US/02962055-3e07-4d93-83ea-6973154d9724/getting-stack-local-variables?forum=netfxbcl
+        /// <summary>
+        /// If the method_name is given, show the details of the method designated
+        /// If the method_name is String.Empty, show the details of the whole call stack.
+        /// The details include the following:
+        /// - method name
+        /// - name and type of the method paramters
+        /// - index and type of local variables
+        /// </summary>
+        /// <param name="method_name"></param>
+        public static void GetCallStackDetails(String method_name)
+        {
+            StackTrace st = new StackTrace();
+            StackFrame[] sfs = st.GetFrames();
+            foreach (StackFrame sf in sfs)
+            {
+                MethodInfo method = sf.GetMethod() as MethodInfo;
+                MethodBody method_body = null;
+                if (method != null)
+                {
+                    method_body = method.GetMethodBody();
+
+                    if (method_name == String.Empty || method.Name.Equals(method_name))
+                    {
+                        Console.WriteLine(new String('-', 60));
+                        Console.WriteLine("Call stack for: " + method.Name);
+                        Console.WriteLine(" Found {0} local variable(s)", method_body.LocalVariables.Count);
+                        Console.WriteLine(" Paramters:");
+                        ParameterInfo[] pis = method.GetParameters();
+                        foreach (ParameterInfo pi in pis)
+                        {
+                            Console.WriteLine(" Name:{0} Type:{1}", pi.Name, pi.ParameterType.ToString());
+                        }
+                        Console.WriteLine(" Local Variables:");
+                        foreach (LocalVariableInfo lvi in method_body.LocalVariables)
+                        {
+                            Console.WriteLine(" Index:{0} Type:{1}", lvi.LocalIndex, lvi.LocalType.ToString());
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(new String('-', 60));
+        }
+
+        public static void GetLocalVariables(Assembly ass, MethodInfo m)
+        {
+            // Assembly ass = Assembly.GetExecutingAssembly();
+            ISymbolReader symreader = SymUtil.GetSymbolReaderForFile(ass.Location, null);
+
+            // MethodInfo m = ass.GetType("PdbTest.TestClass").GetMethod("GetStringRepresentation");
+            ISymbolMethod met = symreader.GetMethod(new SymbolToken(m.MetadataToken));
+
+            int count = met.SequencePointCount;
+
+            ISymbolDocument[] docs = new ISymbolDocument[count];
+            int[] offsets = new int[count];
+            int[] lines = new int[count];
+            int[] columns = new int[count];
+            int[] endlines = new int[count];
+            int[] endcolumns = new int[count];
+
+            met.GetSequencePoints(offsets, docs, lines, columns, endlines, endcolumns);
+
+            Console.WriteLine("The source code for method: " + m.Name);
+            Console.WriteLine(new String('*', 60));
+            // although there's an array of docs, they seem to all have the same value (?) so we'll only use the first one
+            StreamReader reader = new StreamReader(docs[0].URL);
+            string  linesOfCode = reader.ReadToEnd();
+            Console.WriteLine(linesOfCode);
+            reader.Close();
+
+            Console.WriteLine(new String('*', 60));
+            Console.WriteLine();
+            linesOfCode = null;
+        }
 
         public BlackBox() // instantiation does all the interesting stuff
         {
@@ -37,8 +116,16 @@ namespace BlackBoxDemo
                 foreach (System.Reflection.MethodInfo mi in assemblyItem.DeclaredMethods)
                 {
                     {
+                        Console.WriteLine(new String('-', 60));
+                        Console.WriteLine("Looking at method: " + mi.Name);
+                        GetCallStackDetails(mi.Name);
+                        GetLocalVariables(DLL, mi);
+                        LocalVariableNameReader lv = new LocalVariableNameReader(mi);
+
+                        Console.WriteLine();
                         if (mi.Name != "Main") // we'd likely run into a recursion problem by re-invoking Main()
                         {
+                            Console.WriteLine("Invoking: " + mi.Name);
                             mi.Invoke(c, new object[] { });
                         }
                     }
---
+++ BlackBox/LocalVariableReader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Diagnostics;
+using System.Diagnostics.SymbolStore;
+
+namespace BlackBoxDemo
+{
+    public class LocalVariableNameReader
+    {
+        // from https://stackoverflow.com/questions/6166767/retrieve-local-variable-name-from-pdb-files
+        //   "in order for this to work you have to have the pdb of the assembly available. 
+        //   Also please make sure you add a refference to ISymWrapper and your project is targeting .Net 4.0 framework, 
+        //   not .Net 4.0 Client."
+        //
+        // see also https://archive.codeplex.com/?p=ccimetadata
+
+        Dictionary<int, string> _names = new Dictionary<int, string>();
+
+        public string this[int index]
+        {
+            get
+            {
+                if (!_names.ContainsKey(index)) return null;
+                return _names[index];
+            }
+        }
+
+        public LocalVariableNameReader(MethodInfo m)
+        {
+            ISymbolReader symReader = SymUtil.GetSymbolReaderForFile(m.DeclaringType.Assembly.Location, null);
+            ISymbolMethod met = symReader.GetMethod(new SymbolToken(m.MetadataToken));
+            VisitLocals(met.RootScope);
+        }
+
+        void VisitLocals(ISymbolScope iSymbolScope)
+        {
+            foreach (var s in iSymbolScope.GetLocals())
+            {
+                Console.WriteLine("  Found Local Variable: " + s.Name);
+                _names[s.AddressField1] = s.Name;
+            }
+            foreach (var c in iSymbolScope.GetChildren()) VisitLocals(c);
+        }
+    }
+}
