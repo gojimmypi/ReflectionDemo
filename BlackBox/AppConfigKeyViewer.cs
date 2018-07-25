52fcc7a7bbe654e88d250d50fc5fd1a9d5c806b5
gojimmypi <gojimmypi@gmail.com>
2018-07-25 09:54:56 -0700

    Config file viewer now in separate file

---
+++ BlackBox/AppConfigKeyViewer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using System.Reflection;
+using System.Configuration;
+using System.Diagnostics.SymbolStore;
+using System.Runtime.InteropServices;
+
+
+namespace BlackBox
+{
+    public class AppConfigKeyViewer
+    {
+        public static void Show()
+        {
+            Console.WriteLine("Access main application AppSettings[]:");
+            foreach (string key in ConfigurationManager.AppSettings)
+            {
+                Console.WriteLine(" key: " + key + "; value = " + ConfigurationManager.AppSettings[key]);
+            }
+            Console.WriteLine();
+        }
+        public AppConfigKeyViewer()
+        {
+        }
+    }
+}
+
